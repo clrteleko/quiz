5dc53c69e77e7aa66d6e9ae043e9af5a9db80a85
CRISTOBAL LOPEZ <clrteleko@yahoo.es>
2015-07-22 19:29:32 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,26 @@ exports.load = function(req, res, next, quizId){
 
 // GET/quizes
 exports.index = function(req, res){
-  models.Quiz.findAll().then(
-    function(quizes){
-      res.render('quizes/index', {quizes: quizes});
-    }
-  ).catch(function(error){next(error);})
+  // Si no viene parámetro searchText, hay que mostrarlas todas
+  if(req.query.searchText === undefined){
+    models.Quiz.findAll().then(
+      function(quizes){
+        res.render('quizes/index', {quizes: quizes});
+      }
+    ).catch(function(error){next(error);})
+  }
+  // En otro caso, es decir, si viene parámetro searchText, se hace la búsqueda
+  else
+  {
+    // se hace la búsqueda como nos indican en la tarea pedida, es decir, siguiendo
+    // el formato findAll({where: ["pregunta like ?", search]}], delimitando search con el
+    // comodín % antes y después, y cambiando los espacios por %. Además se usa la opción
+    // order para que las entradas encontradas vengan ordenadas en orden ascendente.
+    models.Quiz.findAll({where: ["pregunta like ?","%" + req.query.searchText.replace(" ","%") + "%"],
+                                order:'pregunta ASC'}).then(function(quizes) {
+      res.render('quizes/index', { quizes: quizes});
+   }).catch(function(error) { next(error); })
+  }
 };
 
 // GET /quizes/:id
