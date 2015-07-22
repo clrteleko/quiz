1248015a26c4e61ab3f425e79ab325bc3bff0cdb
CRISTOBAL LOPEZ <clrteleko@yahoo.es>
2015-07-22 16:57:12 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -3,13 +3,13 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6]||null);
-var user = (url[2]||null);
-var pwd = (url[3]||null);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
 var protocol = (url[1]||null);
-var dialect = (url[1]||null);
-var port = (url[5]||null);
-var host = (url[4]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
 var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
@@ -37,9 +37,10 @@ sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function(count){
     if(count === 0) {  // la tabla se inicializa solo si está vacía
-      Quiz.create({pregunta: 'Capital de Italia',
-                   respuesta: 'Roma'
-                  })
+      Quiz.bulkCreate(
+        [{pregunta: 'Capital de Italia', respuesta: 'Roma'},
+         {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ])
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
